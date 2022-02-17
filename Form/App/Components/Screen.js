b30983f2144ebce0a87daaed8a566d7e38864b45
Siddharth <siddharthgovindbhai.parmar@my.jcu.edu.au>
2022-02-18 00:10:52 +1100

    made the screen.js
    Screen.js would make the apps recognise the platform(android, ios) and set the height of the content accordingly.

---
+++ Form/App/Components/Screen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Constants from "expo-constants";
+import { StyleSheet, SafeAreaView, View } from "react-native";
+
+import React from 'react';
+
+function Screen({ children }) {
+    return
+    <SafeAreaView style={styles.screen}>
+        <View>{children}</View>
+    </SafeAreaView>
+
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        paddingTop: Constants.statusBarHeight,
+        flex: 1,
+    },
+});
+export default Screen;
