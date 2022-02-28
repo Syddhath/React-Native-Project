5955a96d8b3cfc1865c9165a12ae6bb5af25b820
Siddharth <siddharthgovindbhai.parmar@my.jcu.edu.au>
2022-02-28 16:16:32 +1100

    Image Selector
    select image from camera roll

--- React_2/App.js
+++ React_2/App.js
@@ -1,9 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Screen from './app/components/Screen'
 import * as ImagePicker from 'expo-image-picker'
+import * as Permission from 'expo-permissions'
+import { Button, Image } from 'react-native';
 
   
 export default function App(){
+const [imageUri, setImageUri] = useState();
+
+
   const requestPermission = async () => {
     const result = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!result.granted)
@@ -12,7 +17,22 @@ export default function App(){
 
   useEffect (() => {
     requestPermission();
-  }, [])
+  }, []);
 
-  return <Screen></Screen>
+  const selectImage = async () => {
+    try {
+      
+      const result = await ImagePicker.launchImageLibraryAsync();
+      setImageUri(result.uri)
+    } catch (error) {
+      console.log('erro')
+    }
+  }
+
+
+  return <Screen>
+    <Button title='Select Image' onPress={selectImage}/>
+    <Image source={{uri: imageUri}} style= {{width: 200, height: 200}}/>
+   
+  </Screen>
 }
