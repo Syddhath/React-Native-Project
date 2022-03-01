2c99a218cb7536658e4a7e33c7ada29a8166467a
Siddharth <siddharthgovindbhai.parmar@my.jcu.edu.au>
2022-03-01 14:58:33 +1100

    Delete Image
    Deleting Image and Alerting

--- React_2/App.js
+++ React_2/App.js
@@ -1,38 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import Screen from './app/components/Screen'
-import * as ImagePicker from 'expo-image-picker'
-import * as Permission from 'expo-permissions'
-import { Button, Image } from 'react-native';
+import React, { useEffect, useState } from "react";
+import Screen from "./app/components/Screen";
 
-  
-export default function App(){
-const [imageUri, setImageUri] = useState();
+import * as Permission from "expo-permissions";
+import { Button, Image } from "react-native";
+import ImageInput from "./app/components/ImageInput";
 
+export default function App() {
+  const [imageUri, setImageUri] = useState();
 
   const requestPermission = async () => {
     const result = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!result.granted)
-    alert('you need permission')
-  }
+    if (!result.granted) alert("you need permission");
+  };
 
-  useEffect (() => {
+  useEffect(() => {
     requestPermission();
   }, []);
 
   const selectImage = async () => {
     try {
-      
       const result = await ImagePicker.launchImageLibraryAsync();
-      setImageUri(result.uri)
+      setImageUri(result.uri);
     } catch (error) {
-      console.log('erro')
+      console.log("erro");
     }
-  }
+  };
 
-
-  return <Screen>
-    <Button title='Select Image' onPress={selectImage}/>
-    <Image source={{uri: imageUri}} style= {{width: 200, height: 200}}/>
-   
-  </Screen>
+  return (
+    <Screen>
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
+    </Screen>
+  );
 }
