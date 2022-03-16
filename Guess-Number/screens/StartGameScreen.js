b272379b9f8f87e8611ec84dc0e99d1a0aff2924
Siddharth <siddharthgovindbhai.parmar@my.jcu.edu.au>
2022-03-16 17:15:55 +1100

    start game

--- Guess-Number/screens/StartGameScreen.js
+++ Guess-Number/screens/StartGameScreen.js
@@ -10,6 +10,7 @@ import {
 } from "react-native";
 import Card from "../components/Card";
 import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
 import Colors from "../constants/colors";
 
 const StartGameScreen = (props) => {
@@ -37,12 +38,19 @@ const StartGameScreen = (props) => {
     setConfirmed(true);
     setEnteredValue("");
     setSelectedNumber(chosenNumber);
+    Keyboard.dismiss();
   };
 
   let confirmedOutput;
 
   if (confirmed) {
-    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>;
+    confirmedOutput = (
+      <Card style={styles.chosenNumber}>
+        <Text>You Selected</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button title="START GAME" />
+      </Card>
+    );
   }
 
   return (
@@ -114,6 +122,10 @@ const styles = StyleSheet.create({
   input: {
     width: 50,
     textAlign: "center",
+  },
+  chosenNumber: {
+    marginTop: 20,
+    alignItems: "center",
   },
 });
 
