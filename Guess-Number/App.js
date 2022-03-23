66b1a1e53ca5e669a89ea58673d8a0a648f15bec
Siddharth <siddharthgovindbhai.parmar@my.jcu.edu.au>
2022-03-23 16:10:33 +1100

    adding fonts and images

--- Guess-Number/App.js
+++ Guess-Number/App.js
@@ -4,10 +4,19 @@ import Header from "./components/Header";
 import GameOverScreen from "./screens/GameOverScreen";
 import GameScreen from "./screens/GameScreen";
 import StartGameScreen from "./screens/StartGameScreen";
+import * as Font from "expo-font";
+
+const fetchFont = () => {
+  Font.loadAsync({
+    "open-sans": require("../Guess-Number/assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-Bold": require("../Guess-Number/assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
@@ -23,6 +32,14 @@ export default function App() {
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  content = (
+    <GameOverScreen
+      roundNumber={1}
+      userNumber={1}
+      onRestart={configureNewGameHandler}
+    />
+  );
 
   if (userNumber && guessRounds <= 0) {
     content = (
--- Guess-Number/screens/GameOverScreen.js
+++ Guess-Number/screens/GameOverScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 
 function GameOverScreen(props) {
   return (
     <View style={styles.screen}>
       <Text>Game Over</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/success.png")}
+          style={styles.image}
+          resizeMode="contain"
+        />
+      </View>
       <Text>Number of Round: {props.roundNumber}</Text>
       <Text>Number was: {props.userNumber}</Text>
       <Button title="New Game" onPress={props.onRestart} />
@@ -17,6 +24,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  imageContainer: {
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    marginVertical: 30,
   },
 });
 
